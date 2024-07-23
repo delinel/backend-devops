5d6e5159ed7d8743efad949e23508e5eec5be35c
delinel <lowedelinel@hotmail.com>
2024-07-23 19:34:11 +0000

    deploy firstly

---
+++ src/test/java/org/it4innov/ProposalControllerTest.java
@@ -0,0 +1,36 @@
+package org.it4innov;
+
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.Assert;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+
+ class ProposalControllerTest {
+
+    @Test
+    void testHelloEndpoint() {
+  /*given()
+          .when().get("/proposal")
+          .then()
+             .statusCode(200);*/
+
+
+        Assert.assertTrue(true);
+    }
+
+
+    @Test
+    void testHelloEndpoint2() {
+  /*given()
+          .when().get("/proposal")
+          .then()
+             .statusCode(200);*/
+
+
+        Assert.assertTrue(true);
+    }
+}
